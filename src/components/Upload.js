21330bd659242867ed2aab8aa4bb5d10355638f9
Emily <etlin4@gmail.com>
2023-04-05 13:43:10 -0500

    clean up unused functions

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,7 +4,6 @@ import Button from '@mui/material/Button';
 import toast, { Toaster } from 'react-hot-toast';
 
 export default function Upload() {
-  const [file, setFile] = useState();
   const [show, setShow] = useState();
   const [episode, setEpisode] = useState();
 
@@ -22,44 +21,6 @@ export default function Upload() {
     console.log('successfully retrieved ', url);
     setUploadURL(url);
   };
-
-  // const fileInputRef = useRef();
-
-  // const handleFileSelected = (file) => {
-  //   console.log('Selected file:', file);
-  //   // Process the selected file, e.g., upload it to your server or read its contents
-  // };
-
-  // const handleButtonClick = () => {
-  //   if (fileInputRef.current) {
-  //     fileInputRef.current.click();
-  //   }
-  //   uploadFile();
-  // };
-
-  // const handleFileChange = (e) => {
-  //   if (e.target.files && e.target.files.length > 0) {
-  //     handleFileSelected(e.target.files[0]);
-  //   }
-  // };
-
-  // const uploadFile = async (e) => {
-  //   const showName = e.target.show.value;
-  //   const episodeName = e.target.episode.value;
-  //   const S3URL = await retrieveURL();
-  //   const file = e.target.file.value;
-  //   console.log(file);
-  //   const upload = await fetch(S3URL, {
-  //     method: 'PUT',
-  //     body: fileInputRef,
-  //   });
-  //   if (upload.ok) {
-  //     toast.success('Uploaded successfully!');
-  //   } else {
-  //     toast.error('Upload failed.');
-  //   }
-  //   setFile(null);
-  // };
 
   const [selectedFile, setSelectedFile] = useState();
   const [uploadURL, setUploadURL] = useState();
@@ -113,44 +74,8 @@ export default function Upload() {
           onClick={retrieveURL}
           onChange={changeHandler}
         />
-
         <input type="submit" value="Upload"></input>
       </form>
     </>
   );
 }
-/*
-const [selectedFile, setSelectedFile] = useState();
-
-const changeHandler = (event) => {
-  setSelectedFile(event.target.files[0]);
-  setIsFilePicked(true);
-};
-
-const handleSubmission = () => {
-  const formData = new FormData();
-
-  formData.append('File', selectedFile);
-
-  fetch('S3URL', {
-    method: 'put',
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log('Success:', result);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-};
-
-return (
-  <div>
-    <input type="file" name="file" onChange={changeHandler} />
-    <div>
-      <button onClick={handleSubmission}>Submit</button>
-    </div>
-  </div>
-);
-*/
